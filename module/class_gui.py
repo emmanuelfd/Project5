080f6c660707df62588bf12b0224b584f20016ce
manufd <boulotmanu@hotmail.com>
2018-04-25 23:03:30 +0100

    select aliment + substitution

--- module/class_gui.py
+++ module/class_gui.py
@@ -9,8 +9,8 @@ class user_input:
 
 
     @classmethod
-    def from_input(cls):
-        return cls(input('saisir le chiffre de la categorie: '))
+    def from_input(cls,table):
+        return cls(input('saisir le chiffre ' + str(table) + ' : '))
 
 
     def check_from_db(self,request_from_db):
@@ -32,12 +32,31 @@ class user_input:
 
 
 
-#def check_from_db(request_from_db,user_input):
-#    for element in request_from_db:
-#        if int(user_input) in element:
-#            check = True
-#            break
-#        else:
- #           check= False
-  #  return check
+class aliment_gui:
+    """ to be used when working on aliment, loading from API and inserting in DB
+    """
+    def __init__(self, aliment):
+        self.id = aliment[0][0]
+        self.name = aliment[0][1]
+        self.nutrition = aliment[0][2]
+        self.store = aliment[0][3]
+        self.url = aliment[0][4]
+        self.category = aliment[0][5]
+       # self.comment =
+
+
+    def substitution_prompt(self):
+        if self.nutrition == 'a':
+            prompt = 'Rien a dire ceci est un bon aliment'
+        elif self.nutrition == 'b':
+            prompt = 'Pas mal mais essayons de trouver mieux'
+        else:
+            prompt = 'attention a votre sante ! Achetez ceci a la place'
+
+        return prompt
+
+
+    #def substitution(self):
+     #   if self.nutrition
+
 
--- user_gui.py
+++ user_gui.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 
-from module.class_gui import user_input
+from module.class_gui import user_input, aliment_gui
 from module.classe_db import DataBaseConnection
 
 
@@ -20,35 +20,71 @@ input_enter = True
 
 while input_enter:
 
-    user_choice = user_input.from_input()
+    user_choice_categorie = user_input.from_input('categorie')
 
 
-    if user_choice.check_from_db(list_category) and user_choice.check_int():
+    if user_choice_categorie.check_from_db(list_category) and user_choice_categorie.check_int():
         input_enter = False
     else:
         continue
 
 
 
-print('vous avez choisi ' + str(user_choice.choice))
+print('vous avez choisi ' + str(user_choice_categorie.choice))
 
 
 ######################
 
 
-list_aliment = connection.fetchalll("select * from aliment where IdCategorie=" + str(user_choice.choice))
+list_aliment = connection.fetchalll("select * from aliment where IdCategorie=" + str(user_choice_categorie.choice))
 
 print(list_aliment)
-print ('##############')
+#print ('##############')
+#print(str(user_choice.choice))
+
+#print ('##############')
+#print(list_aliment)
+#print ('##############')
 
 for x in list_aliment:
     print(str(x[0]) + '##' + str(x[1]))
 
 
+input_enter = True
+
+while input_enter:
+
+
+    user_choice_aliment = user_input.from_input('aliment')
+
+
+    if user_choice_aliment.check_from_db(list_aliment) and user_choice_aliment.check_int():
+        input_enter = False
+    else:
+        continue
+
+print('vous avez choisi ' + str(user_choice_aliment.choice))
+
+aliment_from_db = connection.fetchalll("select * from aliment where Id=" + str(user_choice_aliment.choice))
+
+
+aliment_select = aliment_gui(aliment_from_db)
+print ('##############')
+
+
+
+print('indice nutritionel est  ' + aliment_select.nutrition)
+print(aliment_select.substitution_prompt())
+
+print ('******************')
+for x in list_aliment:
+    if str(x[2]) < aliment_select.nutrition and str(x[2]) != "Unkmown":
+        print(str(x[0]) + '##' + str(x[1]) + '##' + str(x[2]) + '##' + str(x[3]))
+        break
+
+print ('##############')
 
 
 
 
-#-----------------------
 
-choice_aliment = input("saisir le chiffre de l'aliment")
